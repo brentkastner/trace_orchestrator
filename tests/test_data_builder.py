0feae1c2e96d58a3d4d4b9fd52d45dfb8b1990bf
Brent Kastner <brentkastner@gmail.com>
2024-09-17 11:45:59 -0300

    fixing broken tests from requiredCapabilities merged in #5 adding stub flask route test

--- tests/test_data_builder.py
+++ tests/test_data_builder.py
@@ -1,22 +1,24 @@
+import json
 from src.app import dataBuilder
 
 trace = "fdkjsjfldsjfsdj"
 trace_w_tag = "tag:runMe"
+requiredCapabilities =  json.dumps({"requiredCapabilities": [{"browserName": "chrome"}]})
 
 def test_basic_data_conditon():
-    data = dataBuilder(trace, "http://localhost")
+    data = dataBuilder(trace, "http://localhost", requiredCapabilities)
     assert data['reporters'][0]['webhook']['url'] == "http://localhost"
 
 def test_basic_data_condition_does_not_include_tages():
-    data = dataBuilder(trace, "http://localhost")
+    data = dataBuilder(trace, "http://localhost", requiredCapabilities)
     assert 'tags' not in data
 
 def test_basic_tag_case():
-    data = dataBuilder(trace_w_tag, "http://localhost")
+    data = dataBuilder(trace_w_tag, "http://localhost", requiredCapabilities)
     assert 'tags' in data
     assert data['tags'][0] == "runMe"
 
 def test_basic_tag_case_case_insensitive():
-    data = dataBuilder("TAG:runMe", "http://localhost")
+    data = dataBuilder("TAG:runMe", "http://localhost", requiredCapabilities)
     assert 'tags' in data
     assert data['tags'][0] == "runMe"
