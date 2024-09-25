a4501241def5bb71f78c0119cd82052a5d4b2c1d
Brent Kastner <brentkastner@gmail.com>
2024-09-25 12:59:54 -0300

    adding route tests for the various conditions and checking the values

--- tests/test_flask_routes.py
+++ tests/test_flask_routes.py
@@ -15,14 +15,15 @@ def clean_db():
     connection.close()
 
 @pytest.fixture(autouse = True)
-def setup(request):
-    #print('\nbefore: {}'.format(request.node.name))
+def setup():
     print("Cleaning DB for Run")
     clean_db()
-    yield
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    yield connection
     print("Cleaning DB after run")
+    connection.close()
     clean_db()
-    #print('\nafter: {}'.format(request.node.name))
 
 def test_index_route():
     response = app.test_client().get('/')
@@ -30,14 +31,44 @@ def test_index_route():
     assert response.status_code == 200
     assert response.data.decode('utf-8') == 'Hello, Trace Orchestrator!'
 
-def test_insert_runs_without_browsers_specified():
+def test_insert_runs_without_browsers_specified(setup):
     data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs"]}
-    print(f"{json.dumps(data)}")
     response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA', data=json.dumps(data), headers={'Content-Type': 'application/json'})
     assert response.status_code == 200
+
+    runs = setup.execute('SELECT * FROM runs').fetchall()
+    assert len(runs) == 1
+    assert runs[0]['requiredCapabilities'] == json.dumps([{"browserName": "chrome"}])
 
 def test_fails_with_500_wo_projectid():
     data = {"runOrder": ["fdfsdfdsfsdfsdfs"]}
     print(f"{json.dumps(data)}")
     response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA', data=json.dumps(data), headers={'Content-Type': 'application/json'})
     assert response.status_code == 500
+
+def test_insert_multuple_traces_and_tags(setup):
+    data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs", "fdsfdsfsdfsdfds", "fdsfdsfdsfsdfsdfs"]}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    assert response.status_code == 200
+
+    runs = setup.execute('SELECT * FROM runs').fetchall()
+    assert len(runs) == 3
+    assert runs[0]['requiredCapabilities'] == json.dumps([{"browserName": "chrome"}])
+
+def test_insert_runs_with_browser_specified(setup):
+    data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs", "fdsfdsfsdfsdfds", "fdsfdsfdsfsdfsdfs"], "requiredCapabilities": [{"browserName": "firefox"}]}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    assert response.status_code == 200
+
+    runs = setup.execute('SELECT * FROM runs').fetchall()
+    assert len(runs) == 3
+    assert runs[0]['requiredCapabilities'] == json.dumps([{"browserName": "firefox"}])
+
+def test_project_id_is_saved(setup):
+    data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs"]}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    assert response.status_code == 200
+
+    runs = setup.execute('SELECT * FROM runs').fetchall()
+    assert len(runs) == 1
+    assert runs[0]['projectID'] == "dsfdsfdsfs"
