f6411bb70c5e3e74e97089f7744607670bb0d1fb
Georgij Lazarevski <georgij.lazarevski@tarmac.io>
2024-09-26 10:50:57 +0200

    Add more tests involving secret key

--- tests/test_flask_routes.py
+++ tests/test_flask_routes.py
@@ -77,7 +77,7 @@ def test_project_id_is_saved(setup):
     runs = setup.execute('SELECT * FROM runs').fetchall()
     assert len(runs) == 1
     assert runs[0]['projectID'] == "dsfdsfdsfs"
-    
+
 def test_scheduleRun_with_key(setup):
     data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs"]}
     response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA?key=testkey', data=json.dumps(data), headers={'Content-Type': 'application/json'})
@@ -85,6 +85,52 @@ def test_scheduleRun_with_key(setup):
     assert len(runs) == 1
     assert response.status_code == 200
     assert runs[0]['secretKey'] == "testkey"
+
+def test_scheduleRun_with_key_and_multiple_traces(setup):
+    data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs", "fdsfdsfsdfsdfds", "fdsfdsfdsfsdfsdfs"]}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA?key=testkey', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    runs = setup.execute('SELECT * FROM runs').fetchall()
+    assert len(runs) == 3
+    assert response.status_code == 200
+    assert runs[0]['secretKey'] == "testkey"
+    assert runs[1]['secretKey'] == "testkey"
+    assert runs[2]['secretKey'] == "testkey"
+
+def test_scheduleRun_with_key_and_multiple_traces_and_browsers(setup):
+    data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs", "fdsfdsfsdfsdfds", "fdsfdsfdsfsdfsdfs"], "requiredCapabilities": [{"browserName": "firefox"}]}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA?key=testkey', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    runs = setup.execute('SELECT * FROM runs').fetchall()
+    assert len(runs) == 3
+    assert response.status_code == 200
+    assert runs[0]['secretKey'] == "testkey"
+    assert runs[1]['secretKey'] == "testkey"
+    assert runs[2]['secretKey'] == "testkey"
+    assert runs[0]['requiredCapabilities'] == json.dumps([{"browserName": "firefox"}])
+    assert runs[1]['requiredCapabilities'] == json.dumps([{"browserName": "firefox"}])
+    assert runs[2]['requiredCapabilities'] == json.dumps([{"browserName": "firefox"}])
+
+def test_fail_with_500_scheduleRun_with_key_and_without_projectID():
+    data = {"runOrder": ["fdfsdfdsfsdfsdfs"]}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA?key=testkey', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    assert response.status_code == 500
+
+def test_fail_with_500_scheduleRun_with_key_and_without_runOrder():
+    data = {"projectID": "dsfdsfdsfs"}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA?key=testkey', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    assert response.status_code == 500
+
+def test_fail_with_500_scheduleRun_with_key_and_without_data():
+    data = {}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA?key=testkey', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    assert response.status_code == 500
+
+def test_scheduleRun_with_empty_key(setup):
+    data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs"]}
+    response = app.test_client().post('/scheduleRun/wKFv06_08lboHBo7l5NJIA?key=', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    runs = setup.execute('SELECT * FROM runs').fetchall()
+    assert len(runs) == 1
+    assert response.status_code == 200
+    assert runs[0]['secretKey'] == ''
 
 def test_scheduleRun_without_key(setup):
     data = {"projectID": "dsfdsfdsfs", "runOrder": ["fdfsdfdsfsdfsdfs"]}
